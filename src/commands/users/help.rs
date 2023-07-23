ded12196f45b11526bcb3bc330251c42d7731645
Olivia <oliviazhang874@gmail.com>
2023-07-22 23:29:04 -0400

    edited help.rs

--- src/commands/users/help.rs
+++ src/commands/users/help.rs
@@ -4,8 +4,9 @@ pub fn help() -> String {
     let part1: &str = "**List of available commands:**";
     let part2: &str = "`/help`: Shows a list of available commands.";
     let part3: &str = "`/userinfo`: Displays your username and points.";
+    let part4: &str = "`/ask_question`: Asks a question.";
     
-    format!("{}\n\t{}\n\t{}\n", part1, part2, part3)
+    format!("{}\n\t{}\n\t{}\n\t{}\n", part1, part2, part3, part4)
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
