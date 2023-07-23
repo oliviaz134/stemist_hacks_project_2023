42abac189d145382803446805d6c67076058325b
Joshua Estes <jne0010@uah.edu>
2023-07-22 23:09:30 -0500

    Added json struct

--- src/json_structs/config.rs
+++ src/json_structs/config.rs
@@ -1,5 +1,22 @@
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+pub struct Options {
+    pub a: String,
+    pub b: String,
+    pub c: String,
+    pub d: String
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Question {
-    
+    pub id: i64,
+    pub question: String,
+    pub options: Options,
+    pub answer: String
+}
+ 
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub questions: Vec<Question>
+}
