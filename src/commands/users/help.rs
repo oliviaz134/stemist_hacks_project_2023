af6d039d0247b00bd77d7793ce13f3284ea3897e
Olivia <oliviazhang874@gmail.com>
2023-07-23 13:29:31 -0400

    edited help command

--- src/commands/users/help.rs
+++ src/commands/users/help.rs
@@ -6,11 +6,12 @@ pub fn help() -> (CreateEmbed, Option<[String; 4]>, Option<[String; 4]>) {
     let part2: &str = "`/help`: Shows a list of available commands.";
     let part3: &str = "`/userinfo`: Displays your username and points.";
     let part4: &str = "`/ask_question`: Asks a question.";
+    let part5: &str = "`/leaderboard': Displays the leaderboard.";
 
     let mut embed = CreateEmbed::default();
     embed
         .title("Help Menu")
-        .description(format!("{}\n\t{}\n\t{}\n\t{}\n", part1, part2, part3, part4))
+        .description(format!("{}\n\t{}\n\t{}\n\t{}\n\t{}\n", part1, part2, part3, part4, part5))
         .color(0x00ff00);
 
     return (embed, None, None);
